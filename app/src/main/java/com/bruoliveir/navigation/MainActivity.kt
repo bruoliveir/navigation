5b4b3db124f83355fcc6168149a9dd7ce657b206
Bruno Oliveira <bru.oliveir@gmail.com>
2019-08-02 18:42:52 -0300

    Uses Kotlin-specific navigation dependencies and adds action bar support to navigation

--- app/src/main/java/com/bruoliveir/navigation/MainActivity.kt
+++ app/src/main/java/com/bruoliveir/navigation/MainActivity.kt
@@ -2,18 +2,32 @@ package com.bruoliveir.navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy { findNavController(R.id.fNavHost) }
+    private val appBarConfiguration by lazy { AppBarConfiguration(navController.graph) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBar()
         setupTitleFromDestination()
     }
 
+    override fun onSupportNavigateUp() =
+        navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
+    private fun setupActionBar() {
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
     private fun setupTitleFromDestination() {
-        Navigation.findNavController(this, R.id.fNavHost).addOnDestinationChangedListener { controller, _, _ ->
+        navController.addOnDestinationChangedListener { controller, _, _ ->
             title = controller.currentDestination?.label
         }
     }
